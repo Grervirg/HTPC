59a4cf96cdf579fdfd37fccdeedbfe03eef40231
git <grervirg@gmail.com>
2014-12-10 13:53:17 -0500

    0.0.0.1
    Init Htpc Automation and Editor

---
+++ README.md
@@ -0,0 +1,3 @@
+Kodi Htpc Automation Scripts
+=============================
+Version 0.0.0.2
---
+++ shell/scrapemovies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Updates and scrapes movie files
+# version 0.0.0.2
+
+sh /opt/tinymedia/tinyMediaManagerCMD.sh -updateMovies -scrapeNew
