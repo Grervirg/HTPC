bbae0a7a2ae3e96adae2104c8cefd98f867e4b60
Grervirg <grervirg@gmail.com>
2014-12-13 12:48:43 -0500

    0.0.0.2
    added configurations

--- shell/scrapemovies.sh
+++ shell/scrapemovies.sh
@@ -3,3 +3,4 @@
 # version 0.0.0.2
 
 sh /opt/tinymedia/tinyMediaManagerCMD.sh -updateMovies -scrapeNew
+ curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' http://localhost:9191/jsonrpc
