f232472b84f17cad0b8a7cbcf134ca70372914b6
Grervirg <grervirg@gmail.com>
2014-12-21 18:34:36 -0500

    0.0.0.5

--- shell/listmovietypes.sh
+++ shell/listmovietypes.sh
@@ -4,11 +4,11 @@
 #Vars
 #===========================================================
 #Path to Movie Directory
-mpath = /media/MyMedia/
+mpath = '/media/MyMedia'
 
-avilog = /media/MyMedia/Avilog.log
-mp4log = /media/MyMedia/Mp4log.log
-mkvlog = /media/MyMedia/Mkvlog.log
+avilog = '/media/MyMedia/Avilog.log'
+mp4log = '/media/MyMedia/Mp4log.log'
+mkvlog = '/media/MyMedia/Mkvlog.log'
 
 
 find $mpath -type f -name "*.avi" > $avilog
