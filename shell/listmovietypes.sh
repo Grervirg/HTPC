700072246450bd9a35c95ed427903cbf99ffbaab
Grervirg <grervirg@gmail.com>
2014-12-21 18:29:30 -0500

    0.0.0.5
    List Movies By Filetypes

---
+++ shell/listmovietypes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Logs File by Type
+
+#Vars
+#===========================================================
+#Path to Movie Directory
+mpath = /media/MyMedia
+
+avilog = /media/MyMedia/Avilog.log
+mp4log = /media/MyMedia/Mp4log.log
+mkvlog = /media/MyMedia/Mkvlog.log
+
+
+find $mpath -type f -name "*.avi" > $avilog
+find $mpath -type f -name "*.mp4" > $mp4log
+find $mpath -type f -name "*.mkv" > $mkvlog
