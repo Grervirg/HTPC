e0c90a690b3985c1d5e65e5f53715e69db0b28e8
Grervirg <grervirg@gmail.com>
2014-12-27 02:30:17 -0500

    0.0.0.7

--- shell/ConvertMoveFiles.sh
+++ shell/ConvertMoveFiles.sh
@@ -3,7 +3,7 @@
 compath='/home/grervirg/Videos/Complete'
 conpath='/media/MediaProcess'
 sconpath='/mnt/mediacenter'
-dpath='//10.0.1.100/Downloads/Complete/'
+dpath='//10.0.1.100/Downloads/Complete'
 dconpath='/mnt/downloads/'
 ppath='/home/grervirg/Videos'
 
@@ -14,9 +14,9 @@ mmv $compath/Tv/*.* $dconpath/Tv/
 mmv $compath/Movies/*.* $dconpath/Movies/
 rm $ppath/Tv/*.*
 rm $ppath/Movies/*.*
-mmv $ppath/Tv/*.* $sconpath/Tv/
-mmv $ppath/Movies/*.* $sconpath/Movies/
-mmv $ppath/Android/*.* $sconpath/Android/
+mmv $sconpath/Tv/*.* $ppath/Tv/
+mmv $sconpath/Movies/*.* $ppath/Movies/
+mmv $sconpath/Android/*.* $ppath/Android/
 
 
 umount $sconpath
