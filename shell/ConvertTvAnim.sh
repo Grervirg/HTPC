9d8947837640825a46f8dd6f372dd963231ad49e
Grervirg <grervirg@gmail.com>
2014-12-27 03:45:45 -0500

    0.0.0.7

--- shell/ConvertTvAnim.sh
+++ shell/ConvertTvAnim.sh
@@ -3,9 +3,9 @@ spath='/home/grervirg/Videos/Tv/'
 cpath='/home/grervirg/Videos/Complete/Tv/'
 
 find $spath -iname \*.mkv -exec HandBrakeCLI -i {} -o {}.mkv '--preset=\"Kodi Anim\"'  _ {} \;
-mmv *.mkv.mkv #1.mkv
 find $spath -iname \*.avi -exec HandBrakeCLI -i {} -o {}.mkv '--preset=\"Kodi Anim\"'  _ {} \;
-mmv *.avi.mkv #1.mkv
-mmv *.mkv $cpath*.mkv
 find $spath -iname \*.mp4 -exec HandBrakeCLI -i {} -o {}.mkv '--preset=\"Kodi Anim\"'  _ {} \;
-mmv *.mp4.mkv #1.mkv
+cd $spath
+rename \'s/\*.mkv.mkv\' *.mkv
+rename \'s/\*.avi.mkv\' *.mkv
+rename \'s/\*.mp4.mkv\' *.mkv
