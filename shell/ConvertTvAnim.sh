1b1c24304b968a203e793a9a72b2d0bd8712393c
Grervirg <grervirg@gmail.com>
2014-12-26 22:29:16 -0500

    0.0.0.6

--- shell/ConvertTvAnim.sh
+++ shell/ConvertTvAnim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /home/grerivrg/Videos/Tv -iname \*.avi -exec HandBrakeCLI -i {} -o /home/grervirg/Videos/Complete/Tv/${0%.avi}.mkv --preset=\"Kodi Anim\"  _ {} \;
+find /home/grervirg/Videos/Tv -iname \*.avi -exec HandBrakeCLI -i {} -o /home/grervirg/Videos/Complete/Tv/${0%.avi}.mkv --preset=\"Kodi Anim\"  _ {} \;
