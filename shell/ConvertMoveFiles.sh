4b57a5e7279dc3b330d4bfe5fe933df29354e022
Grervirg <grervirg@gmail.com>
2014-12-27 02:15:37 -0500

    0.0.0.7

--- shell/ConvertMoveFiles.sh
+++ shell/ConvertMoveFiles.sh
@@ -7,16 +7,16 @@ dpath='//10.0.1.100/Downloads/Complete/'
 dconpath='/mnt/downloads/'
 ppath='/home/grervirg/Videos/'
 
-mount -t cifs -o username=grervirg,password=omawiq13 //10.0.1.100/MediaProcess $sconpath
-mount -t cifs -o username=grervirg,password=omawiq13 $dpath $dconpath
+mount -t cifs -o username=grervirg,password=omawiq13,uid=1001,gid=1001 //10.0.1.100/MediaProcess $sconpath
+mount -t cifs -o username=grervirg,password=omawiq13,uid=1001,gid=1001 $dpath $dconpath
 mmv $compath/Tv/*.* $dconpath/Tv/
 
 mmv $compath/Movies/*.* $dconpath/Movies/
-rm $sconpath/Tv/*.*
-rm $sconpath/Movies/*.*
-mmv $sconpath/Tv/*.* $sconpath/Tv/
-mmv $conpath/Movies/*.* $sconpath/Movies/
-mmv $conpath/Android/*.* $sconpath/Android/
+rm $ppath/Tv/*.*
+rm $ppath/Movies/*.*
+mmv $ppath/Tv/*.* $sconpath/Tv/
+mmv $ppath/Movies/*.* $sconpath/Movies/
+mmv $ppath/Android/*.* $sconpath/Android/
 
 
 umount $sconpath
