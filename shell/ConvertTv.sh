fd3c51098c0b8b6cd83a3f996c45d6239d1db00a
Grervirg <grervirg@gmail.com>
2014-12-26 22:41:25 -0500

    0.0.0.6

--- shell/ConvertTv.sh
+++ shell/ConvertTv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /home/grervirg/Videos/Tv -iname \*.avi -exec HandBrakeCLI -i {} -o /home/grervirg/Videos/Complete/Tv/${1%.avi}.mkv --preset=\"Kodi\"  _ {} \;
+find /home/grervirg/Videos/Tv -iname \*.avi -exec HandBrakeCLI -i {} -o /home/grervirg/Videos/Complete/Tv/${2%.avi}.mkv --preset=\"Kodi\"  _ {} \;
