73103a497d22010128519f5425996f8fbf1ce0c5
Grervirg <grervirg@gmail.com>
2014-12-27 20:13:03 -0500

    0.0.0.8

--- shell/ConvertMoveFiles.sh
+++ shell/ConvertMoveFiles.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 compath='/home/grervirg/Videos/Complete'
-#conpath='/media/MediaProcess'
+conpath='/media/MediaProcess'
 sconpath='/mnt/mediacenter'
 dconpath='/media/downloads/Complete'
 ppath='/home/grervirg/Videos'
-conpath='nfs://10.0.1.100/media/MediaProcess'
 
 mv $compath/Tv/* $dconpath/tv/
 mv $compath/Movies/* $dconpath/movies/
@@ -13,9 +12,7 @@ mv $compath/Android/* $dconpath/android/
 rm $ppath/Tv/*
 rm $ppath/Movies/*
 rm $ppath/Android/*
-cd $ppath/Tv
-wget -r  $conpath/Tv/*
-#mv $conpath/Tv/* $ppath/Tv/
+rsync -r -v $conpath/Tv/* $ppath/Tv/
 #mv $conpath/Movies/* $ppath/Movies/
 #mv $conpath/Android/* $ppath/Android/
 
