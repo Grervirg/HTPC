b712cc21c3910412902f7d326d148e90fdb978e2
Grervirg <grervirg@gmail.com>
2014-12-28 01:19:33 -0500

    0.0.0..9

--- shell/ConvertVideo.sh
+++ shell/ConvertVideo.sh
@@ -12,62 +12,77 @@ filmpreset='--preset=\"Kodi\"'
 animpreset='--preset=\"KodiAnim\"'
 androidpreset='--preset=\"Android\"'
 
-
+usage() {
+	echo "Usage: $0 [option]"
+	echo "-1 = Tv Film"
+	echo "-2 = Tv Animation"
+	echo "-3 = Movie Film"
+	echo "-4 = Movie Animation"
+	echo "-5 = Android"
+	echo "-6 = DVD Coming soon"
+	exit 1;}
+		
+convert() {
+	echo $spath
+	echo $cpath
+	echo $preset
+	find $spath -iname \*.mkv -exec HandBrakeCLI -i {} -o {}.mkv $preset  _ {} \;
+	find $spath -iname \*.avi -exec HandBrakeCLI -i {} -o {}.mkv $preset  _ {} \;
+	find $spath -iname \*.mp4 -exec HandBrakeCLI -i {} -o {}.mkv $preset  _ {} \;
+	cd $spath
+	mv *.mkv.mkv $cpath
+	mv *.avi.mkv $cpath
+	mv *.mp4.mkv $cpath
+	cd $cpath
+	rename -v 's/\.mkv.mkv$/\.mkv/' *.mkv
+	rename -v 's/\.avi.mkv$/\.mkv/' *.mkv
+	rename -v 's/\.mp4.mkv$/\.mkv/' *.mkv
+	;
+}
 
 
 while getopts ":h:1:2:3:4:5:6:" arg; do
 case $arg in
 	h)
-		echo "Usage: $0 [option]"
-		echo "-1 = Tv Film"
-		echo "-2 = Tv Animation"
-		echo "-3 = Movie Film"
-		echo "-4 = Movie Animation"
-		echo "-5 = Android"
-		echo "-6 = DVD Coming soon"
+		usage
 		;;
 	1)
 		spath=$tpath
 		cpath=$tcompath
 		preset=$filmpreset
+		convert
 		;;
 	2)
 		spath=$tpath
 		cpath=$tcompath
 		preset=$animpreset
+		convert
 		;;
 	3)
 		spath=$mpath
 		cpath=$mcompath
 		preset=$filmpreset
+		convert
 		;;
 	4)
 		spath=$mpath
 		cpath=$mcompath
 		preset=$animpreset
+		convert
 		;;
 	5)
 		spath=$apath
 		cpath=$acompath
 		preset=$androidpreset
+		convert
+		;;
+	*)
+		usage
 		;;
 	esac
 done
 
-echo $spath
-echo $cpath
-echo $preset
 
 
 
-find $spath -iname \*.mkv -exec HandBrakeCLI -i {} -o {}.mkv $preset  _ {} \;
-find $spath -iname \*.avi -exec HandBrakeCLI -i {} -o {}.mkv $preset  _ {} \;
-find $spath -iname \*.mp4 -exec HandBrakeCLI -i {} -o {}.mkv $preset  _ {} \;
-cd $spath
-mv *.mkv.mkv $cpath
-mv *.avi.mkv $cpath
-mv *.mp4.mkv $cpath
-cd $cpath
-rename -v 's/\.mkv.mkv$/\.mkv/' *.mkv
-rename -v 's/\.avi.mkv$/\.mkv/' *.mkv
-rename -v 's/\.mp4.mkv$/\.mkv/' *.mkv
+
